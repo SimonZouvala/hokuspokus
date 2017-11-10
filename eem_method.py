87ce0bf888d716f07d203260374036880a57d977
Šimon Zouvala <445475@mail.muni.cz>
2017-11-10 08:32:39 +0100

    3th commit, upload progres, set class Atom, not more

--- eem_method.py
+++ eem_method.py
@@ -1,107 +1,144 @@
 import sys
 import argparse
 import math
-#import numpy
-
+import numpy as np
 
 from collections import Counter, namedtuple
 
 
-def get_sort_atoms_by_number_and_bond(molecules):
-    atoms_sort, atoms_coordinate = [], []
-    for name, atoms, bonds, coordinate in molecules:
-        number_data, atoms_data = {}, []
-        for i in range(1, len(atoms) + 1):
-            number_data[i] = 0
-        for i in range(len(bonds)):
-            first_atom, second_atom, bond = bonds[i]
-            number_data[first_atom] = max(number_data[first_atom], bond)
-            number_data[second_atom] = max(number_data[second_atom], bond)
-        for key in number_data:
-            atoms_data.append((key, atoms[key - 1], number_data[key],
-                               coordinate[key - 1]))
-            atoms_coordinate.append((key, coordinate[key - 1]))
-        atoms_sort.append((name, len(atoms), atoms_data))
-    print(atoms_coordinate)
-    return atoms_sort, atoms_coordinate
+class Parametrs:
+
+    def set_kappa(self, kappa):
+        self.kappa = kappa
+
+    def __init__(self, atom, parametr_a, parametr_b):
+        self.atom = atom
+        self.parametr_a = parametr_a
+        self.parametr_b = parametr_b
+
+    pass
 
 
-def load_file(file_molecules, file_parametrs):
-    molecule, name, atoms_parametrs = [], "", []
-    Bond = namedtuple('Bond', ['first_atom', 'second_atom', 'bond'])
-    with open(file_parametrs, "r") as fp:
-        for line in fp:
-            if " Kappa=" in line:
-                Kappa = float(line[line.index("Kappa") + len("Kappa=\""):-3])
-            if "<Element" in line:
-                atom = line[line.index("Name") + len("Name=\"")]
-                parametrs = []
-            if "<Bond" in line:
-                parametr_A = int(line.index("A=") + len("A=\""))
-                parametr_B = int(line.index("B=") + len("B=\""))
-                parametrs.append((float(line[parametr_A:parametr_A + 6]),
-                                float(line[parametr_B:parametr_B + 6])))
-            if "</Element>" in line:
-                atoms_parametrs.append((atom, parametrs))
-    with open(file_molecules, "r") as fh:
-        while True:
-            atoms, bonds, coordinate, line = [], [], [], fh.readline()
-            if "" == line[0:1]:
-                return molecule, Kappa, atoms_parametrs
-            name = (line[:].strip())
-            for i in range(2):
-                fh.readline()
-            line = fh.readline()
-            count_atoms, count_bonds = int(line[0:3]), int(line[3:6])
-            for i in range(count_atoms):
-                line = fh.readline()
-                atoms.append(line[31:32])
-                coordinate.append((float(line[3:10]), float(line[13:20]),
-                                       float(line[23:30])))
-            for i in range(count_bonds):
-                line = fh.readline()
-                bonds.append(Bond(int(line[1:3]), int(line[3:6]),
-                                  int(line[8:9])))
+class Atom:
+    def __init__(self, i, element_symbol, bond, coordinate):
+        self.element_symbol = element_symbol
+        self.coordinate = coordinate
+        self.bond = bond
+        self.i = i
+
+    def set_bond(self, bond):
+        self.bond = bond
+
+    def __str__(self):
+        return str("{} {} {} {}".format(self.i, self.element_symbol, self.bond, self.coordinate))
+
+
+class Molecule:
+    #atoms: list[Atom]
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_sort_atoms_by_number_and_bond(self, data):
+        atoms_sort, atoms_coordinate = [], []
+        for name, atoms, bonds, coordinate in data:
+            number_data, atoms_data = {}, []
+            for i in range(1, len(atoms) + 1):
+
+
+                atoms_coordinate.append((key, coordinate[key - 1]))
+            atoms_sort.append((name, len(atoms), atoms_data))
+        return atoms_sort, atoms_coordinate
+    pass
+
+
+class MoleculesSet:
+    #molecules:  list[Molecule]
+
+    def load_from_sdf(self, filename):
+        Bond = namedtuple('Bond', ['first_atom', 'second_atom', 'bond'])
+        i = 0
+        with open(filename, "r") as fh:
             while True:
-                line = fh.readline()
-                if "$$$$" in line:
-                    molecule.append((name, atoms, bonds, coordinate))
+                number_data, atoms, bonds, coordinate, line = {}, [], [], [], fh.readline()
+                if "" == line[0:1]:
                     break
+                name = (line[:].strip())
+                for i in range(2):
+                    fh.readline()
+                line = fh.readline()
+                count_atoms, count_bonds = int(line[0:3]), int(line[3:6])
+                for i in range(count_atoms):
+                    line = fh.readline()
+                    i += 1
+                    number_data[i] = 0
+                    atoms.append(line[31:32])
+                    coordinate.append((float(line[3:10]), float(line[13:20]),
+                                float(line[23:30])))
+                for i in range(count_bonds):
+                    line = fh.readline()
+                    first_atom, second_atom, bond = int(line[1:3]), int(line[3:6]), int(line[8:9])
+                    number_data[first_atom] = max(number_data[first_atom], bond)
+                    number_data[second_atom] = max(number_data[second_atom], bond)
+                for key in number_data:
+                    atom = Atom(key, atoms[key - 1], number_data[key], coordinate[key - 1])
+                    print(atom)
+                while True:
+                    line = fh.readline()
+                    if "$$$$" in line:
+                        molecule = Molecule(name)
+                        i = 0
+                        break
+
+    def load_parametrs(self, filename):
+        with open(filename, "r") as fp:
+            for line in fp:
+                if " Kappa=" in line:
+                    kappa = float(line[line.index("Kappa") + len("Kappa=\""):-3])
+                if "<Element" in line:
+                    element = line[line.index("Name") + len("Name=\"")]
+                if "<Bond" in line:
+                    parametr_a = int(line.index("A=") + len("A=\""))
+                    parametr_b = int(line.index("B=") + len("B=\""))
+                    a = line[parametr_a + 1:parametr_a + 5]
+                    b = line[parametr_b + 1:parametr_b + 5]
+                if "</Element>" in line:
+                    parametrs = Parametrs(element, a, b)
+                    parametrs.set_kappa(kappa)
+    pass
 
 
 def calculate_atom_charge(atoms_sort, Kappa, atoms_parametrs, atoms_coordinate):
     atom_parametr, dx, dy, dz = {}, [], [], []
-    distanse = {}
     for atom, parametr in atoms_parametrs:
-        print (parametr)
         parametr_A, parametr_B = parametr[0]
         atom_parametr[atom, "A"] = parametr_A
         atom_parametr[atom, "B"] = parametr_B
     for name, count, atoms in atoms_sort:
+        element = []
+        identity_matrix = np.eye(count+1)
+        distance = np.zeros((count + 1, count + 1))
+        parametrsA = np.zeros((count+1, 1))
         for data in atoms:
             number, atom, bond, coordinate = data
             cx, cy, cz = coordinate
+            element.append(atom)
             dx.append(cx)
             dy.append(cy)
             dz.append(cz)
+        for i in range(len(element)):
+            parametrsA[i, 0] = atom_parametr[element[i], "A"]
         for i in range(count):
             for j in range(count):
-                """distanse[name, i] += Kappa * math.sqrt((
+                distance[i, j] = Kappa * math.sqrt((
                     (dx[j] - dx[i]) ** 2) + ((dy[j] - dy[i]) ** 2)
                      + ((dz[j] - dz[i]) ** 2))
-                """
-                distanse[i, j] = Kappa * math.sqrt((
-                    (dx[j] - dx[i]) ** 2) + ((dy[j] - dy[i]) ** 2)
-                     + ((dz[j] - dz[i]) ** 2))
-                if j == count - 1:
-                    distanse[i, count] = -1
-                if i == count - 1:
-                    distanse[count, j] = 1
                 if i == j:
-                    distanse[i, j] = atom_parametr[atoms[i][1], "B"]
-        distanse[count, count] = 0
-        print(name, distanse)
-        distanse = {}
+                    distance[i, j] = atom_parametr[atoms[i - 1][1], "B"]
+        distance[count, :] = 1
+        distance[:, count] = -1
+        distance[count, count] = 0
+        print(name, distance)
         dx, dy, dz = [], [], []
 
 
@@ -113,12 +150,18 @@ def main():
     parser.add_argument("Parametrs file", help="Give a file with parametr ",
                         type=str)
     args = parser.parse_args()
+    mset = MoleculesSet()
+    mset.load_parametrs(sys.argv[2])
+    mset.load_from_sdf(sys.argv[1])
+
+
+"""
     file_molecules, file_parametrs = sys.argv[1], sys.argv[2]
     molecules, Kappa, atoms_parametrs = load_file(file_molecules,
                                                   file_parametrs)
     atoms_sort, atoms_coordinate = get_sort_atoms_by_number_and_bond(molecules)
     calculate_atom_charge(atoms_sort, Kappa, atoms_parametrs, atoms_coordinate)
-
+"""
 
 if __name__ == "__main__":
     main()
