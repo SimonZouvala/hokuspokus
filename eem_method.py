82fcd6afa4a66a7f6cad78bbf2d1165032a76b6c
Šimon Zouvala <445475@mail.muni.cz>
2017-11-27 22:22:30 +0100

    9th commit: Sine lapsu. Now draw graph with all elements and their chargers with parameters B.

--- eem_method.py
+++ eem_method.py
@@ -2,8 +2,57 @@ import sys
 import argparse
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 from collections import Counter
+
+
+class Graph:
+    def __init__(self, first_data):
+        self.first_data = first_data
+
+    def prepare_data(self):
+        data, data_ready = [], []
+        occurrence = Counter()
+        for name, count, atoms, charges, parameters in self.first_data:
+            for i, atom in enumerate(atoms):
+                element, number, bond = atom
+                element_name = element + "" + str(bond)
+                occurrence[element_name] += 1
+                data.append((element_name, charges[i], parameters[element, "B", bond]))
+        data = sorted(data)
+        start = 0
+        for element in sorted(occurrence):
+            prepare_data = []
+            for i in range(start, occurrence[element] + start):
+                element_name, charge, parameter = data[i]
+                prepare_data.append((charge, parameter))
+            data_ready.append((element, prepare_data))
+            start += occurrence[element]
+        self.data_for_draw = data_ready
+
+    def draw_data(self):
+        markers = [".", ",", "o", "v", "^", "<", ">", "p", "P", "*", "h", "+", "x", "X", "D", "d", "|", "1", "2", "3"]
+        mark = 0
+        for element, coordinate in sorted(self.data_for_draw):
+            x, y,  = [], []
+            for x_data, y_data in coordinate:
+                x.append(x_data)
+                y.append(y_data)
+            plt.scatter(x, y, marker=markers[mark], label=element)
+            mark += 1
+        plt.legend(loc=1)
+        plt.show()
+
+    def draw_graph(self):
+        for name, count, atoms, charges, parameters in self.first_data:
+            x = np.linspace(0, count + 1, count + 1)
+            for i in range(count):
+                print(charges[i])
+                y = x + charges[i]
+        fig, ax = plt.subplots()
+        ax.scatter(x, y, 'ro')
+        plt.show()
 
 
 class Atom:
@@ -127,16 +176,16 @@ class MoleculesSet:
                     self.error_molecules += 1
                     print("Missing parameters for {}. element {}({}) in {}. Program did not count with this "
                           "element.".format(atom.number, atom.element_symbol, atom.bond, name))
-                    output.append((name, count, atom.element_symbol, atom.bond))
+                    output.append((name, "error", atom.element_symbol, 0, atom_parameter))
                     continue
                 distance[count, :] = 1
                 distance[:, count] = -1
                 distance[count, count] = 0
                 charges = np.linalg.solve(distance, parameters_a)
-                output.append((name, count, data_from_atoms, charges))
+                output.append((name, count, data_from_atoms, charges, atom_parameter))
         except KeyError:
             print("Something wrong with calculate")
-        output_to_file(self.filename, self.file_parameters, output)
+        self.data_for_graph = output_to_file(self.filename, self.file_parameters, output)
 
     def get_statistic_from_set(self):
         elements, count_element, count_all_atoms, elements_in_molecules = [], Counter(), 0, []
@@ -172,25 +221,29 @@ class MoleculesSet:
             a_parameter, b_parameter = parameter[0]
             print("{:>3}{}: {:8} {:7}".format(element, type_bond, a_parameter, b_parameter))
 
+    def get_data_for_graph(self):
+        return self.data_for_graph
+
     def __str__(self):
         return str("{}".format(self.molecules))
 
 
 def output_to_file(filename, file_parameters, data):
+    data_for_graph = []
     new_file = "data_from_" + filename + "_with_parameters_" + file_parameters
     with open(new_file, "w") as f:
-        for name, count, atoms, charges in data:
-            print("{}\n{}".format(name, count), file=f)
-            for i, atom in enumerate(atoms):
-                try:
+        for name, count, atoms, charges, parameters in data:
+            try:
+                print("{}\n{}".format(name, int(count)), file=f)
+                for i, atom in enumerate(atoms):
                     element, number, bond = atom
-                    print("{0:6d}  {1}{2:2} {3: f}".format(number, element, bond, charges[i]), file=f)
-                except ValueError:
-                    print("Missing parameter for this element {}({}). "
-                          "Program can not calculate charges.".format(atoms, charges), file=f)
-                    continue
-            print("", file=f)
+                    print("{0:6d}  {1:>2}{2} {3: f}".format(number, element, bond, charges[i]), file=f)
+                data_for_graph.append((name, count, atoms, charges, parameters))
+            except ValueError:
+                continue
+            print("$$$$\n", file=f)
     print("Now you can find charge for each element in file {}".format(new_file))
+    return data_for_graph
 
 
 def get_distance(kappa, first_coordinate, second_coordinate):
@@ -206,16 +259,22 @@ def main():
     parser.add_argument("Second_Parameters_file", help="Give a file with another parameters in xml format", type=str,
                         nargs="?", default=0)
     args = parser.parse_args()
+
     mset = MoleculesSet()
     mset.load_parameters(args.Parameters_file)
     mset.load_from_sdf(args.SDF_file)
     mset.calculate_atom_charge()
     mset.get_statistic_from_set()
+    data1 = mset.get_data_for_graph()
     mset.get_statistic_from_parameters()
     if args.Second_Parameters_file:
         mset.load_parameters(args.Second_Parameters_file)
         mset.calculate_atom_charge()
         mset.get_statistic_from_parameters()
+
+    draw = Graph(data1)
+    draw.prepare_data()
+    draw.draw_data()
 
 
 if __name__ == "__main__":
