04ed856c38a04171673d829fcf67579cb39d9dbb
Šimon Zouvala <445475@mail.muni.cz>
2017-11-29 15:30:06 +0100

    As separate output application

---
+++ output.py
@@ -0,0 +1,19 @@
+class Output:
+    def __init__(self, filename, file_parameters, data):
+        data_for_graph = []
+        self.new_file = "result/data_from_" + filename.strip("data/") + "_with_parameters_"\
+                        + file_parameters.strip("data/")
+        with open(self.new_file, "w") as f:
+            for name, count, atoms, charges, parameters in data:
+                try:
+                    print("{}\n{}".format(name, int(count)), file=f)
+                    for i, atom in enumerate(atoms):
+                        element, number, bond = atom
+                        print("{0:6d}  {1:>2}{2} {3: f}".format(number, element, bond, charges[i]), file=f)
+                    data_for_graph.append((name, count, atoms, charges, parameters))
+                except ValueError:
+                    continue
+        print("Now you can find charge for each element in file {}".format(self.new_file))
+
+    def give_name_file_with_result(self):
+        return self.new_file
