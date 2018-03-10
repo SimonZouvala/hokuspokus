2af441b1dd345ff8b61f351cfe5202f7eebcbfd5
Šimon Zouvala <445475@mail.muni.cz>
2018-03-10 22:25:49 +0100

    MGCHM maybe okay

--- mgchm.py
+++ mgchm.py
@@ -1,6 +1,4 @@
-import math
 import numpy as np
-from numpy.linalg import inv
 
 
 class Calculate:
@@ -13,33 +11,14 @@ class Calculate:
                 degree_matrix = molecule.count_bond_matrix
                 connectivity_matrix = molecule.bond_matrix
                 identity_matrix = np.eye(count)
-                print("D = Degree matrix (OK) ")
-                print(degree_matrix)
-                print("A = Connectivity matrix (OK)")
-                print(connectivity_matrix)
-                print("I = Identity matrix (OK)")
-                print(identity_matrix)
                 simplified_matrix = degree_matrix - connectivity_matrix + identity_matrix
-                print("S = Simplified matrix (OK)")
-                print(simplified_matrix)
                 pt_electronegativity = np.zeros((count, 1))
                 for i, atom in enumerate(molecule.atoms):
                     pt_electronegativity[i][0] = periodic_table[atom.element_symbol]
                     data_from_atoms.append((atom.element_symbol, atom.bond))
-                print("X° = Periodic table electronegativity (OK)")
-                print(pt_electronegativity)
-                print("Inverse S (OK)")
-                print(inv(simplified_matrix))
-                print("Tady to už nehraje :(")
                 nk_electronegativity = np.linalg.solve(simplified_matrix, pt_electronegativity)
-                print("X = Unknown electronegativities")
-                print(nk_electronegativity)
                 deviation_away_pt = nk_electronegativity-pt_electronegativity
                 geometric_mean = np.sum(nk_electronegativity)/count
-                print("Geometric_mean")
-                print(geometric_mean)
-                print("X - X°")
-                print(deviation_away_pt)
                 charges = deviation_away_pt * (1/geometric_mean)
                 print("Q = Charges")
                 print(charges)
@@ -60,9 +39,3 @@ class Calculate:
 
     def give_result(self):
         return self.output
-
-
-def get_distance(kappa, first_coordinate, second_coordinate):
-    x1, y1, z1 = first_coordinate
-    x2, y2, z2 = second_coordinate
-    return kappa / (math.sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2) + ((z1 - z2) ** 2)))
